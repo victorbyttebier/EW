ae1c59c14bdd42b70691944eedbbe9e71e9cd115
victorbyttebier <victor.byttebier@student.howest.be>
2018-12-05 09:57:24 +0100

    canvas-img tile on marker (multiple) --part1

--- src/js/shuffle.js
+++ src/js/shuffle.js
@@ -1,9 +1,10 @@
 {
-  const dog = document.querySelector(`#dogImage`);
-
   const boardSize = 480,
         tileCount = 3,
         tileSize = boardSize / tileCount;
+
+  const markerImages = document.querySelectorAll(`#image`);
+
 
   let boardParts, tile = 0;
   let canvas = [], imageData = [], images = [];
@@ -13,16 +14,41 @@
     image.src = '../assets/dog.jpg';
     image.width = boardSize;
     image.height = boardSize;
-    image.addEventListener('load', () => createTiles(image), false);
+    image.addEventListener('load', () => createTiles(image));
     setBoard();
   }
+
+  /*
+  Ter info:
+  - in createTiles maken we onder andere de nieuwe canvassen (createCanvas) voor de stukjes.
+  - in createTileImage (hieronder) ontvangen we de coördinaten van de canvas stukjes.
+  We zetten we de nieuwe geshuffelde canvassen om in een image. In de array images zit pngdata. console.log(images);
+
+  De data uit de array images moet de nieuwe src worden in de a-markers > img id="image" data-tile="1" SRC="assets/dog.jpg"
+  De 9 markers die nu in de html staan heb ik tussen in de files gestoken zodat je het kan testen (uitprinten).
+  Nadien we stukjes mooi erop krijgen, kunnen we onze custom markers in onze webtoepassing verwerken.
+  bv. markerImages[0].src = images[0] AKA markerImages[tile].src = images[tile]
+
+  PROBLEEM TO SOLVE: De createTileImage wordt 9 keer uitgevoerd. Er worden 9 dezelfde stukjes getoond op de marker.
+  */
 
   const createTileImage = (image, {imgdata}) => {
     const {x, y} = imgdata;
     canvas[tile].getContext("2d").drawImage(image, x * tileSize, y * tileSize, tileSize, tileSize, 0, 0, tileSize, tileSize);
-    //save canvas as image
-    dog.src = images[0];
     images[tile] = canvas[tile].toDataURL("image/png").replace("image/png", "image/octet-stream");
+
+    if(markerImages[tile].dataset.tile == tile){
+      console.log(images);
+      markerImages[0].src = images[0];
+      markerImages[0].src = images[1];
+      markerImages[0].src = images[2];
+      markerImages[0].src = images[3];
+      markerImages[0].src = images[4];
+      markerImages[0].src = images[5];
+      markerImages[0].src = images[6];
+      markerImages[0].src = images[7];
+      markerImages[0].src = images[8];
+    }
   };
 
   const createCanvas = tile => {
@@ -32,6 +58,7 @@
     canvas[tile].setAttribute(`data-tile`, `${tile}`);
     canvas[tile].setAttribute(`width`, tileSize);
     canvas[tile].setAttribute(`height`, tileSize);
+    //document.querySelector(`a-scene`).appendChild(canvas[tile]);
   };
 
   const createTiles = image => {
@@ -42,7 +69,7 @@
           let x = boardParts[i][j].x;
           let y = boardParts[i][j].y;
           imageData[tile] = {imgdata: {x,y}};
-          createTileImage(image,imageData[tile]);
+          createTileImage(image, imageData[tile]);
 
           tile++;
       }
